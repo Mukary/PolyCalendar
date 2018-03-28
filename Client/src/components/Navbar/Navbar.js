8163ba6b333fa444a53403be5d50e435bbfe9411
Mukary <perso.rahim@gmail.com>
2018-03-28 22:00:14 +0200

    setting up profile page

--- Client/src/components/Navbar/Navbar.js
+++ Client/src/components/Navbar/Navbar.js
@@ -2,13 +2,23 @@ import React from 'react'
 import './Navbar.styles.css'
 
 export default class Navbar extends React.Component {
+  constructor(props){
+    super(props)
+    this.logout = this.logout.bind(this)
+  }
+
+  logout() {
+    window.localStorage.removeItem('pcal_token')
+  }
+
   render(){
     return(
       <ul>
-        <li><a class="active" href="/">Home</a></li>
+        <li><a className="active" href="/">Home</a></li>
         <li><a href="/login">Views</a></li>
         <li><a href="/invite">Calendars</a></li>
         <li><a href="/profile">Profile</a></li>
+        <li><a href="" onClick={this.logout}>Log out</a></li>
       </ul>
     )
   }
